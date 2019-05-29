725313eab48b2757d601d307bace706486d50464
Jeschko <jeschkp2@at-work.local>
2019-05-29 11:08:03 +0200

    added hungry prompt

--- hungry.js
+++ hungry.js
@@ -1,1 +1,13 @@
-console.log("I am hungry");
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Are you hungry? ', (answer) => {
+  // TODO: Log the answer in a database
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+});
